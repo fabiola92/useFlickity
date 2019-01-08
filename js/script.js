6fbc2ff0a3f5b3d69c570d91e0402f793e406c90
Fabiola Jantura <fabiolajantura@gmail.com>
2019-01-08 20:32:05 +0100

    add hash progress bar try add restart button

--- js/script.js
+++ js/script.js
@@ -2,7 +2,28 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  freeScroll: true,
+  pageDots: false,
+  hash: true
+  flkty.next();
+  flkty.select( 3 );
 });
 
 flkty.on( 'scroll', function( progress ) {3});
+
+//restart
+var restartButton = document.querySelector('#restart');
+restartButton.addEventListener('click', function( event ) {
+  console.log('klik')
+  flkty.next();
+});
+
+
+//progress bar
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
